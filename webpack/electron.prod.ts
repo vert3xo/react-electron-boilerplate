c3db1b1d42575b499f27fe2b097ebab44f076d0b
vert3xo <skvertexo@gmail.com>
2022-01-29 16:35:46 +0100

    added terser plugin and bundle analyzer

--- webpack/electron.prod.ts
+++ webpack/electron.prod.ts
@@ -3,6 +3,9 @@ import * as path from "path";
 
 import paths from "./webpacks.paths";
 
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import TerserPlugin from "terser-webpack-plugin";
+
 export default {
   mode: "production",
   target: "electron-main",
@@ -12,5 +15,14 @@ export default {
   output: {
     path: paths.electronBuildPath,
     filename: "[name].js",
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.ANALYZE === "true" ? "server" : "disabled",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({ parallel: true })],
   },
 } as webpack.Configuration;

9b78bae58d2eddd0662de6249e06c1c7533914ad
vert3xo <skvertexo@gmail.com>
2022-01-31 08:22:36 +0100

    created simple preload

--- electron/main.ts
+++ electron/main.ts
@@ -8,9 +8,9 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // webPreferences: {
-    //   preload: path.join(__dirname, "preload.js"),
-    // },
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   // and load the index.html of the app.
